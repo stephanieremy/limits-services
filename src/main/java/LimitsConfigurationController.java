88cd721e20cb07dad6d48138a2754472a7deedeb
stephanie_remy <stephanie_remy@yahoo.fr>
2020-03-27 09:17:39 +0100

    Initialize Limits services

---
+++ src/main/java/LimitsConfigurationController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfig() {
+        return new LimitConfiguration(1000, 1);
+    }
+}
